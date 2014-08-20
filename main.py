72a121319cce165af1daf848e01af6ebb0dbd4f9
DaniloMDias <danilodias222@gmail.com>
2014-08-20 01:23:27 -0300

    Gerenciador de páginas
    Implementei um gerenciador de páginas.
    Também fiz uma página para o login com os devidos campos e com o logo da
    PJ.

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
+
 from kivy.vector import Vector
 from kivy.clock import Clock
+
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-from  kivy.uix.boxlayout import BoxLayout
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.anchorlayout import AnchorLayout
+
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class Demo(GridLayout):
+class Login(Screen):
     pass
+
+class Apresentacao(Screen):
+    pass
+
+class Cadastro(Screen):
+    pass
+
+class Contatos(Screen):
+    pass
+
+class Manager(ScreenManager):
+    pass
+
     
 
 
 class AppPjApp(App):
     def build(self):
-        #layout = Demo()
-        #layout = BoxLayout(orientation='vertical')
-        #
-        #layout.add_widget(Label(text='CADASTRO DE MEMBROS', height = 40, size_hint_y=None, pos_hint_y = None))        
-        #layout.add_widget(TextInput(text='Digite o Nome do Membro', height = 40, size_hint_y=None, pos_hint_y = None))        
-        #layout.add_widget(TextInput(text='Digite o E-Mail do Membro', height = 40, size_hint_y=None, pos_hint_y = None))        
-        #layout.add_widget(TextInput(text='Digite o Telefone do Membro', height = 40, size_hint_y=None, pos_hint_y = None))
-        #layout.add_widget(Button(text='blablabla', center_x = layout.center_x, height = 40, size_hint_y=None, pos_hint_y = None))
         
-        return Demo()
+        
+        return Manager()
     
 if __name__ == '__main__':
     AppPjApp().run()
